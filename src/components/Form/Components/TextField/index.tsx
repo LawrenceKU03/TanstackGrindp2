6a817a52a3ff53f89d52d9d046afc5c9bc4d404d
Lawrence <lawuche29@gmail.com>
2026-07-23 23:56:55 +0100

    info:played around with error handling would swap it out with zod tommorrow onwards

--- src/components/Form/Components/TextField/index.tsx
+++ src/components/Form/Components/TextField/index.tsx
@@ -10,15 +10,27 @@ export type Input = {
 
 const index: React.FC<Input> = ({ name, placeholder, type, form }) => {
 	return (
-		<form.Field name={name}>
+		<form.Field
+			name={name}
+			validators={{
+				onChange: ({ value }: { value: string }) =>
+					!value.includes("@") ? "Invalid email" : undefined,
+			}}
+		>
 			{(field: any) => (
-				<input
-					value={field.state.value}
-					type={type}
-					onChange={(e) => field.handleChange(e.target.value)}
-					placeholder={placeholder}
-					className="w-[100%] px-4 py-2 rounded-md border-none text-indent-2 bg-gray-100 my-2 font-['Space_Grotesk']"
-				/>
+				<>
+					<input
+						value={field.state.value}
+						type={type}
+						onBlur={field.handleBlur}
+						onChange={(e) => field.handleChange(e.target.value)}
+						placeholder={placeholder}
+						className="w-[100%] px-4 py-2 rounded-md border-none text-indent-2 bg-gray-100 my-2 font-['Space_Grotesk']"
+					/>
+					{field.state.meta.isTouched && field.state.meta.errors.length > 0 && (
+						<span>{field.state.meta.errors.join(", ")}</span>
+					)}
+				</>
 			)}
 		</form.Field>
 	);
