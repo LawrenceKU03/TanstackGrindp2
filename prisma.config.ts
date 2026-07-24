43277e0a9c7fb1cf2ef8431d0b004c81bf087a99
Lawrence <lawuche29@gmail.com>
2026-07-24 01:11:49 +0100

    chore:almost done setting up better-auth/prisma/postgresql

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig, env } from 'prisma/config'
+import { config } from "dotenv";
+config({ path: ".env.local" });   // explicit path instead of the default "dotenv/config"
+
 
 export default defineConfig({
   schema: './prisma/schema.prisma',
---
+++ src/lib/auth.ts
@@ -0,0 +1,11 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "#/db";
+
+export const auth = betterAuth({
+	database: prismaAdapter(prisma, {
+		provider: "postgresql", // or "mysql", "postgresql", ...etc
+	}),
+	baseURL: "http://localhost:3000/",
+	emailAndPassword: { enabled: true, requireEmailVerification: false },
+});
